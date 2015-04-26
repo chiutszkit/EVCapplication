d996cfecb6850a83ffa3c21fc76d84f3c5742ca0
aesops0124 <aesops0124@gmail.com>
2015-04-27 01:51:00 +0800

    changed search function at overview_activity

--- app/src/main/java/com/example/tommyhui/evcapplication/overview/OverviewActivity.java
+++ app/src/main/java/com/example/tommyhui/evcapplication/overview/OverviewActivity.java
@@ -2,23 +2,35 @@ package com.example.tommyhui.evcapplication.overview;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
+import android.support.v7.widget.SearchView.OnSuggestionListener;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
-
-import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tommyhui.evcapplication.R;
+import com.example.tommyhui.evcapplication.utils.SearchUtil;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class OverviewActivity extends ActionBarActivity {
 
+    private MenuItem searchItem;
+    private SearchView searchView;
+    private MenuItem result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +48,6 @@ public class OverviewActivity extends ActionBarActivity {
         /*Set Action Bar's Icon*/
         ImageView myImgView = (ImageView)findViewById(R.id.action_bar_icon);
         myImgView.setImageResource(R.drawable.overview_icon);
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,18 +55,110 @@ public class OverviewActivity extends ActionBarActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.overview_options_menu, menu);
 
-        // Get the SearchView and set the overviewSearchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        result = menu.findItem(R.id.overview_status_text);
+        result.setVisible(false);
 
-        MenuItem searchItem = menu.findItem(R.id.overview_action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-//        SearchView searchView = (SearchView) menu.findItem(R.id.overview_action_search);
-        Log.v("test-logging", "R.id.overview_action_search : " +  menu.findItem(R.id.overview_action_search));
+//        // Get the SearchView and set the overview_searchable configuration
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        /*Expand Search Bar*/
+        searchItem = menu.findItem(R.id.overview_action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint(getResources().getString(R.string.overview_search_view_title));
+
+//        Log.v("test-logging", "R.id.overview_action_search : " +  menu.findItem(R.id.overview_action_search));
 
 
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
-//        // Associate overviewSearchable configuration with the SearchView
+            @Override
+            /*When texts are changed*/
+            public boolean onQueryTextChange(String query) {
+                setSuggestionForSearch(query);
+                return true;
+            }
+            @Override
+            /*When texts are submit*/
+            public boolean onQueryTextSubmit(String query) {
+                // TODO Auto-generated method stub
+
+                /*Clear the Query and Display the Result*/
+                searchView.setQuery("", false);
+                searchView.clearFocus();
+                searchView.onActionViewCollapsed();
+                result.setTitle(query);
+                result.setVisible(true);
+
+                Toast.makeText(getApplicationContext(), "Please wait...", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        return true;
+    }
+
+        protected void setSuggestionForSearch(String query) {
+            // TODO Auto-generated method stub
+            ArrayList<String> suggestionArray;
+
+            suggestionArray = getListOfCountries();
+
+            searchView.setSuggestionsAdapter(SearchUtil
+                    .getCursorAdapter(OverviewActivity.this, suggestionArray,
+                            query));
+
+            searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+
+                @Override
+                public boolean onSuggestionClick(int position) {
+
+                    showSearchResult(SearchUtil
+                            .getItemTag((MatrixCursor) searchView
+                                    .getSuggestionsAdapter().getItem(position)));
+                    return false;
+                }
+
+                @Override
+                public boolean onSuggestionSelect(int position) {
+                    return false;
+                }
+            });
+
+        }
+
+        private ArrayList<String> getListOfCountries() {
+            // TODO Auto-generated method stub
+
+            ArrayList<String> items = new ArrayList<String>();
+
+            String[] isoCountries = Locale.getISOCountries();
+            for (String country : isoCountries) {
+                Locale locale = new Locale("en", country);
+        //          String iso = locale.getISO3Country();
+        //          String code = locale.getCountry();
+                String name = locale.getDisplayCountry();
+
+                if (!"".equals(name)) {
+                    items.add(name);
+                }
+            }
+
+            return items;
+
+        }
+        protected void showSearchResult(String itemTag) {
+            // TODO Auto-generated method stub
+
+            /*Clear the Query and Display the Result*/
+            searchView.setQuery("", false);
+            searchView.clearFocus();
+            searchView.onActionViewCollapsed();
+            result.setTitle(itemTag);
+            result.setVisible(true);
+
+            Toast.makeText(getApplicationContext(), "Please wait...", Toast.LENGTH_SHORT).show();
+        }
+
+//        // Associate overview_searchable configuration with the SearchView
 //        SearchManager searchManager =
 //                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
 //        SearchView searchView =
@@ -63,8 +166,7 @@ public class OverviewActivity extends ActionBarActivity {
 //        searchView.setSearchableInfo(
 //                searchManager.getSearchableInfo(getComponentName()));
 
-        return super.onCreateOptionsMenu(menu);
-    }
+
     /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
